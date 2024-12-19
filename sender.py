471951093e85045c1a6138bf91a8f77d54655437
HasanPalito <palitohasan@gmail.com>
2024-12-19 23:27:35 +0700

    port changed

--- sender.py
+++ sender.py
@@ -3,7 +3,7 @@ import cv2
 import websockets
 
 async def send_images():
-    async with websockets.connect("ws://localhost:9999/sender") as websocket:
+    async with websockets.connect("ws://3.129.58.174:9999/sender") as websocket:
         cap = cv2.VideoCapture(0)
 
         try:
@@ -16,21 +16,3 @@ async def send_images():
                 await asyncio.sleep(0.05)
         finally:
             cap.release()
-
-import paho.mqtt.client as mqtt
-
-def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
-    #control logic here
-
-client = mqtt.Client()
-
-client.on_message = on_message
-
-client.connect("localhost", 1883, 60)
-
-topic = "joystick/data"
-client.subscribe(topic)
-client.loop_forever()
-
-asyncio.get_event_loop().run_until_complete(send_images())
